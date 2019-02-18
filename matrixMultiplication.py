021b2bc05014c9e3fd61684e6d7605c1af36c9f5
FanchiniRudolf <43999588+FanchiniRudolf@users.noreply.github.com>
2019-02-18 13:43:44 -0600

    Update matrixMultiplication.py

--- matrixMultiplication.py
+++ matrixMultiplication.py
@@ -1,5 +1,23 @@
-archivo1 = open("filename", "r",)
 
-legend = archivo1.read()
+def openArchive(nombre):
+    archivo1 = open(nombre, "r",)
 
-matriz = legend.split("\n")
+    legend = archivo1.read()
+
+    renglones = legend.split("\n")
+
+    matriz =[]
+    for x in renglones:
+        temp = []
+        temp = x.split(",")
+        for i in range(len(temp)):
+            temp[i]= int(temp[i])
+        matriz.append(temp)
+
+
+    archivo1.close()
+
+    return matriz
+
+
+print(openArchive("01. Matrix_A_16_2_4.txt"))

3ade73878cd50ad22d59bfc7119e9a373f15392a
FanchiniRudolf <43999588+FanchiniRudolf@users.noreply.github.com>
2019-03-28 21:37:53 -0600

    arbol finalizado

--- binaryTreeNumOrder.py
+++ binaryTreeNumOrder.py
@@ -1,10 +1,14 @@
+import turtle
+from tkinter import *
+
+
 class node(object):
-    value= 0
+    val = 0
     left = None
     right = None
 
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, val):
+        self.val = val
 
     def setLeft(self, node):
         self.left = node
@@ -12,9 +16,10 @@ class node(object):
     def setRight(self, node):
         self.right = node
 
+
 def agregarNodo(root, newVal):
     tempNode = node(newVal)
-    if tempNode.value < root.value:
+    if tempNode.val < root.val:
         if root.left == None:
             root.setLeft(tempNode)
         else:
@@ -25,28 +30,61 @@ def agregarNodo(root, newVal):
         else:
             agregarNodo(root.right, newVal)
 
-def impresion (root, arbol):
-    arbol.append(str(root.value))
-
-    left = []
-    right = []
-    if (root.left != None):
-        left.append(impresion(root.left, []))
-    if (root.right != None):
-        right.append(impresion(root.right, []))
-
-    arbol.append(left)
-    arbol.append(right)
-    return arbol
-
-nums = [6, 7, 10, 11, 5, 3, 2]
-
-root = node(nums[0])
-for x in range(1,len(nums)):
-    agregarNodo(root, nums[x])
-
-arbol= impresion(root, [])
-print(arbol)
+def height(root):
+    return 1 + max(height(root.left), height(root.right)) if root else -1
 
 
+def addTree():
+    input = iterEntry.get()
+    nums = (input[1:-1])
+    nodes = nums.split(",")
+    root = node(int(nodes[0]))
+    for x in range(1, len(nodes)):
+        agregarNodo(root, int(nodes[x]))
+    window.destroy()
+    turtleDraw(root)
 
+
+def turtleDraw(root):
+    def jumpto(x, y):
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+
+    def draw(node, x, y, dx):
+        if node:
+            t.goto(x, y)
+            jumpto(x, y - 20)
+            t.write(node.val, align='center', font=('Arial', 12, 'normal'))
+            draw(node.left, x - dx, y - 60, dx / 2)
+            jumpto(x, y - 20)
+            draw(node.right, x + dx, y - 60, dx / 2)
+
+    t=turtle.Turtle()
+    t.speed(0);
+    turtle.delay(0)
+    h = height(root)
+    jumpto(0, 30 * h)
+    draw(root, 0, 30 * h, 40 * h)
+    t.hideturtle()
+    turtle.mainloop()
+
+
+window = Tk()
+window.title("Page Break")
+window.geometry("500x300")
+
+topFrame = Frame(window)
+topFrame["pady"] = 10
+topFrame.pack(side=TOP)
+
+accion = Label(topFrame, text="Arbol en forma:{1,2...n}")
+accion.pack(side=TOP)
+
+iterEntry = Entry(topFrame)
+iterEntry.pack(side=TOP)
+
+btdir1 = Button(topFrame, text="Dibujar Arbol", command=addTree)
+btdir1.pack(side=TOP)
+
+window.mainloop()
